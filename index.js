79125c7cac8ffdf0de0fe16308209862c4fffda8
David Bud <davidbudr@gmail.com>
2023-09-14 12:11:45 +0100

    Corrected error on root route where cyclic.com was referenced instead of cyclic.app

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ app.use('/rpi', rpiSource);
 
 app.get('/', (req,res,next)=>{
     res.send(`<p>to use ukrpix please do the following: <br>
-    use ukrpix.cyclic.com/(indexation)/(year)/ <br>
+    use ukrpix.cyclic.app/(indexation)/(year)/ <br>
     or <br>
-    ukrpix.cyclic.com/(indexation)/(year)/(month) <br>
+    ukrpix.cyclic.app/(indexation)/(year)/(month) <br>
     where (year) is replaced by the year i.e. 2018 <br>
     and (month) is replaced by the either the word or numbered month i.e. October or 10 <br>
     (indexation) is replace by rpi or rpix depending on which you need</p>
+
+    <a href='https://ukrpix.cyclic.app/rpix/2018/10' target='_blank'>Example of usage</a>
     `)
 })
 
